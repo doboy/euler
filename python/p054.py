ceba6a05d951def94bdea297a324e95832940829
Huan Do <doboy0@gmail.com>
2013-12-31 14:03:59 -0800

    comment out inprogress work

--- python/p054.py
+++ python/p054.py
@@ -1,32 +1,32 @@
-# Tags: poker, time consuming
-ranks = [ isRoyalFlush, isStraightFlush, isFourofAKind, isFullHouse,
-          isFlush, isStraight, isThreeofAKind, isTwoPairs, isOnePair, 
-          isHighCard ]
+# # Tags: poker, time consuming
+# ranks = [ isRoyalFlush, isStraightFlush, isFourofAKind, isFullHouse,
+#           isFlush, isStraight, isThreeofAKind, isTwoPairs, isOnePair,
+#           isHighCard ]
 
-game = []
+# game = []
 
-for line in open( "../txt/p054" ):
-    line = line.split()
-    hand1, hand2 = line[:5], line[5:]
-    game.append( ( hand1, hand2 ) )
+# for line in open( "../txt/p054" ):
+#     line = line.split()
+#     hand1, hand2 = line[:5], line[5:]
+#     game.append( ( hand1, hand2 ) )
 
-def rank( hand ):
-    ''' 
-    0 : High Card
-    1 : One Pair
-    2 : Two Pairs
-    3 : Three of a Kind
-    4 : Straight
-    5 : Flush
-    6 : Full House
-    7 : Four of a Kind
-    8 : Straight Flush
-    9 : Royal Flush 
+# def rank( hand ):
+#     '''
+#     0 : High Card
+#     1 : One Pair
+#     2 : Two Pairs
+#     3 : Three of a Kind
+#     4 : Straight
+#     5 : Flush
+#     6 : Full House
+#     7 : Four of a Kind
+#     8 : Straight Flush
+#     9 : Royal Flush
 
-    return rank, highest rank card, highest card'''
-    for rankFn in rankFns:
-        if rankFn( hand ):
-            return rankFn( hand )
+#     return rank, highest rank card, highest card'''
+#     for rankFn in rankFns:
+#         if rankFn( hand ):
+#             return rankFn( hand )
 
-print sum( 1 for hand1, hand2 in game if rank( hand1 ) > rank( hand2 ) )
-    
+# print sum( 1 for hand1, hand2 in game if rank( hand1 ) > rank( hand2 ) )
+
